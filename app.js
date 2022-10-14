40431ca43f5e86309398a4bc8b7201a3f77b789e
Mykola <niksandler@gmail.com>
2022-10-14 23:50:05 +0300

    save

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const logger = require('morgan')
 const cors = require('cors')
+const mongoose = require('mongoose')
+const { DB_HOST } = require('./config')
+
+mongoose.connect(DB_HOST)
+  .then(() => console.log("Database connection successful"))
+  .catch(error => console.log(error.message))
 
 const contactsRouter = require('./routes/api/contacts')
 
