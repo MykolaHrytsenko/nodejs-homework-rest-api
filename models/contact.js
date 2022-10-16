6d05cc89630fad40d7ee638f597c0cdd0bf3f0de
Mykola <niksandler@gmail.com>
2022-10-17 00:09:41 +0300

    ref getAll and getById

--- models/contact.js
+++ models/contact.js
@@ -1,4 +1,5 @@
 const { Schema, model } = require("mongoose")
+const Joi = require("joi")
 
 const { handleSaveErrors } = require("../helpers")
 
@@ -20,11 +21,34 @@ const contactSchema = new Schema(
             type: Boolean,
             default: false,
         },
-    }
+    }, { versionKey: false, timestamps: true }
 )
+
+const addSchema = Joi.object({
+    name: Joi.string().required().messages({
+        'string.base': `"name" should be a type of 'string'`,
+        'any.required': `"name" is a required field`
+    }),
+    email: Joi.string().required().messages({
+        'string.base': `"email" should be a type of 'string'`,
+        'any.required': `"email" is a required field`
+    }),
+    phone: Joi.string().required().messages({
+        'string.base': `"phone" should be a type of 'string'`,
+        'any.required': `"phone" is a required field`
+    }),
+    favorite: Joi.boolean(),
+})
+
+const schemas = {
+    addSchema,
+}
 
 contactSchema.post("save", handleSaveErrors)
 
 const Contact = model("contact", contactSchema)
 
-module.exports = Contact;
+module.exports = {
+    Contact,
+    schemas,
+};
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -4,17 +4,17 @@ const ctrl = require("../../controllers/contacts")
 
 const { ctrlWrapper } = require("../../helpers")
 
-// const { validateBody } = require("../../middlewares")
+const { validateBody } = require("../../middlewares")
 
-// const schemas = require("../../schemas/contacts")
+const { schemas } = require("../../models/contact")
 
 const router = express.Router()
 
-// router.get('/', ctrlWrapper(ctrl.getAll))
+router.get('/', ctrlWrapper(ctrl.getAll))
 
-// router.get('/:contactId', ctrlWrapper(ctrl.getById))
+router.get('/:contactId', ctrlWrapper(ctrl.getById))
 
-router.post('/', ctrlWrapper(ctrl.add))
+router.post('/', validateBody(schemas.addSchema), ctrlWrapper(ctrl.add))
 
 // router.put('/:contactId', validateBody(schemas.addSchema), ctrlWrapper(ctrl.updateById))
 
